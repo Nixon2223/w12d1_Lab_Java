b59fa5e3a3721b7f8700446c1d894afb54210470
Nixon2223 <n.nixon@hotmail.co.uk>
2022-02-14 15:24:56 +0000

    Cash Class - enum for money created

--- src/main/java/Cash.java
+++ src/main/java/Cash.java
@@ -1,2 +1,12 @@
 public class Cash {
+    public enum Money {
+        NOTE5(500),
+        NOTE10(1000);
+
+        private final int value;
+
+        Money(int value) {
+            this.value = value;
+    }
+
 }
